1bd0739e16cf893415fd4259e2c580df4e8f049a
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-04-02 00:32:43 -0600

    Added developer properties as hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+DEVELOPER = {
+  "name" => "Luis Arroyo",
+  "program" => "Backend",
+  "age" => 24,
+  "height_in_ft" => 5.9,
+  "is_frontend" => false,
+  "is_male" => true,
+  "places_lived" => ["Colorado", "Alaska", "Puerto Rico"],
+  "cars_owned" => ["Toyota", "Ford", "Chevorlet", "Subaru"]
+}
