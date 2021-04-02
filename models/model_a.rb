0e436c2cbb04f70164209c5ad706776087de0ade
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-04-02 00:23:40 -0600

    added DINOSOAR hash

---
+++ models/model_a.rb
@@ -0,0 +1,11 @@
+DINOSOAR = {
+  "name" => "T-Rex",
+  "planet" => "Earth",
+  "number_of_legs" => 2,
+  "weight_in_lbs" => 2365.3,
+  "extinct" => true,
+  "herbivore" => false,
+  "featured_movies" => ["Jurrasic Park", "Ice Age"],
+  "diet" => ["Edmontosaurus", "Triceratops"]
+
+}
