c68b799489aa1386d4a0f0ae0dd684427d14e785
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-04-02 00:04:26 -0600

    Added pt.2 program challenges

---
+++ variables.rb
@@ -0,0 +1,13 @@
+movie = "Step Brothers"
+is_entertaining = true
+movie = "GoodFellas"
+
+num_4 = 4
+num_6 = 6
+num_4 * num_6
+product = 24
+
+favorite_foods = ["pizza", "rice", "eggs"]
+favorite_foods. shift
+
+movie.length
