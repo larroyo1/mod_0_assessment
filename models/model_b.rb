b5dbce1d8c392e431567fcfdbadd02d587b45a44
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-04-03 17:35:44 -0600

    fix array names

--- models/model_b.rb
+++ models/model_b.rb
@@ -1,10 +1,10 @@
-DEVELOPER = {
+developer = {
   "name" => "Luis Arroyo",
   "program" => "Backend",
   "age" => 24,
   "height_in_ft" => 5.9,
   "is_frontend" => false,
   "is_male" => true,
-  "places_lived" => ["Colorado", "Alaska", "Puerto Rico"],
-  "cars_owned" => ["Toyota", "Ford", "Chevorlet", "Subaru"]
+  "resided" => ["Colorado", "Alaska", "Puerto Rico"],
+  "owned_vehicles" => ["Toyota", "Ford", "Chevorlet", "Subaru"]
 }
