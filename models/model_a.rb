c03eb5fb91413e40c2bd22585fdb2d468d57f6c5
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-04-03 17:34:50 -0600

    fix boolean names, rename array

--- models/model_a.rb
+++ models/model_a.rb
@@ -1,10 +1,10 @@
-DINOSOAR = {
+dinosoar = {
   "name" => "T-Rex",
   "planet" => "Earth",
   "number_of_legs" => 2,
   "weight_in_lbs" => 2365.3,
-  "extinct" => true,
-  "herbivore" => false,
+  "is_extinct" => true,
+  "is_herbivore" => false,
   "featured_movies" => ["Jurrasic Park", "Ice Age"],
   "diet" => ["Edmontosaurus", "Triceratops"]
 
